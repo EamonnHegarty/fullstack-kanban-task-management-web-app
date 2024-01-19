4eab6023ca5667f01bba656bfd13c3e4d7fa1dd5
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-01-19 14:30:47 +0000

    added controllerss for mvc

---
+++ server/controllers/boardController.js
@@ -0,0 +1,26 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Board from "../models/boardModel.js";
+
+// @desc Fetch all boards
+// @route GET /api/boards
+// @access Public
+const getBoards = asyncHandler(async (req, res) => {
+  const boards = await Board.find({});
+  res.json(boards);
+});
+
+// @desc Fetch a board
+// @route GET /api/boards/:id
+// @access Public
+const getBoardById = asyncHandler(async (req, res) => {
+  const board = await Board.findById(req.params.id);
+
+  if (board) {
+    res.json(board);
+  } else {
+    res.status(404);
+    throw new Error("resource not found");
+  }
+});
+
+export { getBoards, getBoardById };
--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -1,29 +1,9 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Board from "../models/boardModel.js";
+import { getBoards, getBoardById } from "../controllers/boardController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const boards = await Board.find({});
-    res.json(boards);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const board = await Board.findById(req.params.id);
-
-    if (board) {
-      res.json(board);
-    } else {
-      res.status(404);
-      throw new Error("resource not found");
-    }
-  })
-);
+router.route("/").get(getBoards);
+router.route("/:id").get(getBoardById);
 
 export default router;
