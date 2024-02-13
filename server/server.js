75b27da838e29ea5e8b20d5e259290beb6ff5d66
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-02-13 15:39:39 +0000

    created protect middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import boardRoutes from "./routes/boardRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -18,6 +19,7 @@ app.use(cors());
 // Body parse middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
